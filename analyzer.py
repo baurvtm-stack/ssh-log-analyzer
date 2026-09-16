e99c7aa840610b1f5ec8cc4879bbe69293d4c0d6
Bauyrzhan <baurvtm@gmail.com>
2026-09-16 19:29:44 +0500

    Add error handling for missing log file

--- analyzer.py
+++ analyzer.py
@@ -1,15 +1,23 @@
+import sys
+
 counts = {}
 THRESHOLD = 3
-with open("auth.log") as f:
-    for x in f:
-        if "Failed password" in x:
-            words = x.split()
-            idx =words.index("from")
-            ip = words[idx + 1]
-            if ip in counts:
-                counts[ip] += 1
-            else:
-                counts[ip] = 1
+
+try:
+    with open("auth.log.ba") as f:
+        for x in f:
+            if "Failed password" in x:
+                words = x.split()
+                idx =words.index("from")
+                ip = words[idx + 1]
+                if ip in counts:
+                    counts[ip] += 1
+                else:
+                    counts[ip] = 1
+except FileNotFoundError:
+    print("[!] Error: file 'auth.log' not found. Make sure it exists in the current directory.")
+    sys.exit(1)
+
 sorted_ips = sorted(counts.items(),key=lambda pair: pair[1], reverse= True)
 
 print("[!] Suspicious login activity detected:")
